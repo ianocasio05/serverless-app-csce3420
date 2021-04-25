f30c7ec9ae3fac2325bc392e3876a8180f7da447
Ian Ocasio <ianocasio@my.unt.edu>
2021-04-25 15:55:42 -0500

    moving text

--- js/ride.js
+++ js/ride.js
@@ -2,6 +2,7 @@
 
 var WildRydes = window.WildRydes || {};
 WildRydes.map = WildRydes.map || {};
+var AWS = require('aws-sdk');
 
 (function rideScopeWrapper($) {
     var authToken;
@@ -41,13 +42,12 @@ WildRydes.map = WildRydes.map || {};
     function completeRequest(result) {
         var unicorn;
         var pronoun;
-        //what the fuck
+
         console.log('Response received from API: ', result);
         unicorn = result.Unicorn;
         pronoun = unicorn.Gender === 'Male' ? 'his' : 'her';
         var message = unicorn.Name + ', your ' + unicorn.Color + ' unicorn, is on ' + pronoun + ' way! ';
 
-        var AWS = require('aws-sdk');
         /*AWS.config.accessKeyId = 'AKIASKX4SQKU6YCZKT7C';
         AWS.config.secrectAccessKey = 'eO4dyx+TXOjY9Nxl+hF3Evk5S6+GShlG7O8AUzDH';
         AWS.config.region = 'us-east-2';*/
